2a0e38ebb5737e1bcfe11b4e88613d5b61eb8f74
hongdongdong <hongdongdong3012@gmail.com>
2021-02-01 23:40:58 +0900

    commit 15강

---
+++ todolist/src/TodoList.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class TodoList extends Component {
+    render() {
+        <div className="todoListMain">
+            <div className="header">
+                <form>
+                    <input placeholder="enter task"></input>
+                    <button type="submit"></button>
+                </form>
+            </div>
+        </div>
+    }
+}
